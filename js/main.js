8f6c2830ef05b75e372d9aaee03938777af842b2
IMJOne <mompapa7@gmail.com>
2023-01-21 22:20:42 +0900

    Navbar event handling

--- js/main.js
+++ js/main.js
@@ -17,3 +17,34 @@ window.addEventListener('load', () => {
     AOS.init({ duration: 1000 });
   }
 });
+
+// All function of the header & arrow
+const nav = document.querySelector('nav');
+const dark = document.querySelector('.dark');
+const trigger = document.querySelector('.header__trigger');
+const close = document.querySelector('.nav__close');
+const backward = document.querySelector('.header__backward');
+const arrowUpBtn = document.querySelector('.btn__arrow-up');
+
+container.addEventListener('click', e => {
+  const target = e.target;
+
+  switch (target) {
+    case trigger:
+      nav.classList.add('open');
+      dark.classList.add('open');
+      break;
+    case close:
+    case dark:
+      nav.classList.remove('open');
+      dark.classList.remove('open');
+      break;
+    case backward:
+      window.history.back();
+      break;
+    case arrowUpBtn:
+      container.scrollIntoView({ behavior: 'smooth' });
+    default:
+      return;
+  }
+});
