758c50612f14f648399cb92e8e835df42390a13e
IMJOne <mompapa7@gmail.com>
2023-01-21 22:22:52 +0900

    Login form sweet alert

--- js/main.js
+++ js/main.js
@@ -49,6 +49,48 @@ container.addEventListener('click', e => {
   }
 });
 
+// Login form
+const loginForm = document.querySelector('.login__form');
+
+if (loginForm) {
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    if (id.value === '' || pw.value === '') {
+      Swal.fire({
+        title: `입력 정보가\n올바르지 않습니다.`,
+        text: '아이디나 비밀번호를 확인해주세요.',
+        icon: 'question',
+        button: '확인',
+      }).then(() => {
+        id.focus();
+      });
+    } else {
+      Swal.fire('죄송합니다.', '현재 점검 중인 관계로 로그인이 불가합니다', 'warning').then(() => {
+        let timerInterval;
+        Swal.fire({
+          title: '로그인 실패',
+          html: '<b></b> 초 후에 메인 페이지로 이동합니다.',
+          icon: 'error',
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: () => {
+            Swal.showLoading();
+            timerInterval = setInterval(() => {
+              Swal.getHtmlContainer().querySelector('b').textContent = (Swal.getTimerLeft() / 1000).toFixed(0);
+            }, 100);
+          },
+          willClose: () => {
+            clearInterval(timerInterval);
+          },
+        }).then(() => {
+          window.location.href = 'index.html';
+        });
+      });
+    }
+  });
+}
+
 // Show arrow up btn
 document.addEventListener('scroll', () => {
   if (window.scrollY > 500) {
