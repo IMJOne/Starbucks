22eb4b504983f37e79d04e47413eaf4fb6408847
IMJOne <mompapa7@gmail.com>
2023-02-05 02:26:02 +0900

    Fix  API key and oveflow error

--- js/main.js
+++ js/main.js
@@ -5,14 +5,15 @@ const loading = document.querySelector('.loading');
 window.addEventListener('load', () => {
   if (loading) {
     setTimeout(() => {
-      document.body.style.overflow = 'auto';
       loading.style.opacity = 0;
       AOS.init({ duration: 1000 });
       ScrollOut({ threshold: 0.2, once: true });
       Splitting();
       loading.style.display = 'none';
+      document.body.style.overflow = 'auto';
     }, 1000);
   } else {
+    document.body.style.overflow = 'auto';
     AOS.init({ duration: 1000 });
   }
 });
